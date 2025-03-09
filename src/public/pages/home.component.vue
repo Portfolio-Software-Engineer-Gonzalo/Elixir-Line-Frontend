cd0ba97dbae0e077613be92bd98bfe2d251f7c7f
Janover Saldaña <u20201b510@upc.edu.pe>
2025-03-09 12:32:32 -0500

    feat: Added base architecture for customer portfolio and inventory management contexts.

---
+++ src/public/pages/home.component.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "home",
+
+  created() {
+    console.log('Home component created');
+  }
+
+}
+</script>
+
+<template>
+
+  <div class="w-full">
+    <h1>Home</h1>
+    <p>Welcome to ACME Learning Center.</p>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
