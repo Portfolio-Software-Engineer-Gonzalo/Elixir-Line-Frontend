8f23a814d50bca32703c28e3138e34801afcd045
Janover Saldaña <u20201b510@upc.edu.pe>
2025-03-15 12:43:13 -0500

    feat: Added elixir line component and toolbar componente.

---
+++ src/public/components/navbar-elixir-line.component.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: "navbar-elixir-line",
+
+  data() {
+    return {
+      drawer: true,
+      visible: false,
+      items: [
+        { label: "title-home-option", to: "/vitivinicultor/home",                     icon: 'pi pi-home' },
+        { label: "title-inventory-option", to: "/vitivinicultor/inventory",           icon: 'pi pi-receipt' },
+        { label: "title-winemaking-option", to: '/vitivinicultor/winemaking-process', icon: 'pi pi-spinner-dotted' },
+        { label: "title-my-clients-option", to: '/vitivinicultor/clients',            icon: 'pi pi-user' },
+        { label: "title-my-orders-option", to: '/vitivinicultor/orders/history',      icon: 'pi pi-list-check' },
+        { label: "title-my-products-option", to: '/vitivinicultor/products',          icon: 'pi pi-tags' },
+        { label: "title-support-option", to: '/vitivinicultor/Support',               icon: 'pi pi-wrench' },
+      ]
+    }
+  },
+
+  methods: {
+    closeDrawer() {
+      this.visible = false;
+    }
+  },
+
+
+  created() {
+    console.log('HeaderContent created');
+  }
+
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/public/components/toolbar-elixir-line.component.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: "toolbar-elixir-line",
+
+  data() {
+    return {
+      items: [
+        {label: 'sign-in', to: "/home/sign-in", icon: 'pi pi-user'},
+        {label: 'sign-up', to: "/home/sign-up", icon: 'pi pi-user-plus'}
+      ]
+    }
+
+  },
+
+  created()
+  {
+    console.log('Toolbar Home Elixir Line Created');
+  }
+}
+
+</script>
+
+<template>
+
+  <div class="toolbar-elixir-line">
+    <div class="p-toolbar-group-left">
+      <router-link v-for="item in items" :key="item.label" :to="item.to" class="p-button p-button-text">
+        <i :class="item.icon"></i>
+        <span>{{ $t(item.label) }}</span>
+      </router-link>
+    </div>
+
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
