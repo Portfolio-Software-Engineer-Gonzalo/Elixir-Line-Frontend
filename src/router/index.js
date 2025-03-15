65b0988184d8a65084a1fa6a9b2aebffdf0fbb9e
Janover Saldaña <u20201b510@upc.edu.pe>
2025-03-15 12:42:37 -0500

    feat: Added elixir line component and page path not found.

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import PageNotFoundComponent from "../public/pages/page-not-found.component.vue";
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+
+
+        {path: '/page-not-found',       name: 'PageNotFound',             component: PageNotFoundComponent,            meta: { title: 'Page Not Found' }},
+
 
         // Add your routes here as an array of objects with the following properties: path, name, component and meta
         // (optional)
